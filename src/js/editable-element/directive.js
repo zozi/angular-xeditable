19f39faac6a019ee27c68f3be4f42eb2b5308251
Evan Hourigan <evan.hourigan@zozi.com>
2014-07-01 14:22:22 -0700

    enable editable-form-disabled attribute on form to disable automatic form handling behavior

--- src/js/editable-element/directive.js
+++ src/js/editable-element/directive.js
@@ -36,7 +36,7 @@ function($parse, $compile, editableThemes, $rootScope, $document, editableContro
         // element wrapped by form
         if(ctrl[1]) {
           eFormCtrl = ctrl[1];
-          hasForm = true;
+          hasForm = !$document[0].forms[0].attributes.getNamedItem('editable-form-disabled');
         } else if(attrs.eForm) { // element not wrapped by <form>, but we hane `e-form` attr
           var getter = $parse(attrs.eForm)(scope);
           if(getter) { // form exists in scope (above), e.g. editable column
